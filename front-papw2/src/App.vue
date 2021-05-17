0eb42742585e85eeac9cbc2adf914cc3c32ee3cc
Cynthia Torres <cynthia.to16@gmail.com>
2021-05-16 21:25:55 -0500

    Creacion Proyecto y vista Login

---
+++ front-papw2/src/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="page-container md-layout-column">
+    <md-toolbar class="md-primary">
+      <md-button
+        class="md-icon-button"
+        @click="showNavigation = true"
+        v-if="sessionActive"
+      >
+        <md-icon>menu</md-icon>
+      </md-button>
+      <router-link to="/">
+        <span class="md-title">Artic-Noticias</span>
+      </router-link>
+      <div class="md-toolbar-section-end">
+        <md-button v-if="sessionActive">Nombre Usuario</md-button>
+
+        <router-link v-else to="/loginUser"
+          ><md-button>Inicia Sesion</md-button></router-link
+        >
+      </div>
+    </md-toolbar>
+
+    <md-drawer :md-active.sync="showNavigation" md-swipeable>
+      <md-toolbar class="md-transparent" md-elevation="0">
+        <span class="md-title">Opciones</span>
+      </md-toolbar>
+
+      <md-list>
+        <md-list-item>
+          <md-icon>move_to_inbox</md-icon>
+          <span class="md-list-item-text">Inbox</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>send</md-icon>
+          <span class="md-list-item-text">Sent Mail</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>delete</md-icon>
+          <span class="md-list-item-text">Trash</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>error</md-icon>
+          <span class="md-list-item-text">Spam</span>
+        </md-list-item>
+      </md-list>
+    </md-drawer>
+    <router-view></router-view>
+    <!-- <md-drawer class="md-right" :md-active.sync="showSidepanel" md-swipeable>
+      <md-toolbar class="md-transparent" md-elevation="0">
+        <span class="md-title">Favorites</span>
+      </md-toolbar>
+
+      <md-list>
+        <md-list-item>
+          <span class="md-list-item-text">Abbey Christansen</span>
+
+          <md-button class="md-icon-button md-list-action">
+            <md-icon class="md-primary">chat_bubble</md-icon>
+          </md-button>
+        </md-list-item>
+
+        <md-list-item>
+          <span class="md-list-item-text">Alex Nelson</span>
+
+          <md-button class="md-icon-button md-list-action">
+            <md-icon class="md-primary">chat_bubble</md-icon>
+          </md-button>
+        </md-list-item>
+
+        <md-list-item>
+          <span class="md-list-item-text">Mary Johnson</span>
+
+          <md-button class="md-icon-button md-list-action">
+            <md-icon>chat_bubble</md-icon>
+          </md-button>
+        </md-list-item>
+      </md-list>
+    </md-drawer> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data: () => ({
+    showNavigation: false,
+    showSidepanel: false,
+    sessionActive: false,
+  }),
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Material+Icons');
+
+.md-drawer {
+  width: 230px;
+  max-width: calc(100vw - 125px);
+}
+</style>
---
+++ front-papw2/src/views/LoginUser.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="md-layout md-alignment-center">
+    <form novalidate class="md-layout ">
+      <md-card class="md-layout-item md-size-70">
+        <md-card-header>
+          <div class="md-title">Iniciar Sesion</div>
+        </md-card-header>
+        <md-card-content>
+          <md-field>
+            <label for="email">Email</label>
+            <md-input type="email" name="email" id="email" />
+          </md-field>
+          <md-field>
+            <label for="email">Contraseña</label>
+            <md-input type="password" name="password" id="password" />
+          </md-field>
+        </md-card-content>
+        <md-card-actions>
+          <md-button type="submit" class="md-primary">Okey!</md-button>
+        </md-card-actions>
+      </md-card>
+    </form>
+    <form novalidate class="md-layout">
+      <md-card class="md-layout-item md-size-70">
+        <md-card-header>
+          <div class="md-title">Nuevo Usuario</div>
+        </md-card-header>
+
+        <md-card-content>
+          <!-- Nombres y apellido -->
+          <div class="md-layout md-gutter">
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label for="first-name">Nombre:</label>
+                <md-input
+                  name="first-name"
+                  id="first-name"
+                  autocomplete="given-name"
+                />
+              </md-field>
+            </div>
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label for="last-name">Apellido</label>
+                <md-input
+                  name="last-name"
+                  id="last-name"
+                  autocomplete="family-name"
+                />
+              </md-field>
+            </div>
+          </div>
+
+          <div class="md-layout md-gutter">
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label for="age">Telefono</label>
+                <md-input type="tel" id="age" name="age" autocomplete="age" />
+              </md-field>
+            </div>
+          </div>
+
+          <md-field>
+            <label for="email">Email</label>
+            <md-input type="email" name="email" id="email" />
+            <span class="md-error">The email is required</span>
+            <span class="md-error">Invalid email</span>
+          </md-field>
+        </md-card-content>
+
+        <!-- <md-progress-bar md-mode="indeterminate" /> -->
+
+        <md-card-actions>
+          <md-button type="submit" class="md-primary">Create user</md-button>
+        </md-card-actions>
+      </md-card>
+
+      <md-snackbar>The user {{}} was saved with success!</md-snackbar>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormValidation',
+  data: () => ({}),
+  methods: {},
+}
+</script>
+<style scoped>
+.md-progress-bar {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+}
+
+.card-expansion {
+  height: 480px;
+}
+
+.md-card {
+  margin: 0 auto;
+  margin-top: 3rem;
+  margin-bottom: 3rem;
+  display: inline-block;
+  vertical-align: top;
+}
+</style>
